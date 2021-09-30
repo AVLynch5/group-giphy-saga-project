ccf38d7d54e41ebb5c2b0544f853552d4aec302c
ahmedsharur <ahmedshafeysharur@gmail.com>
2021-09-30 14:29:49 -0500

    added reducers and sagas functions

--- src/index.js
+++ src/index.js
@@ -9,34 +9,40 @@ import { takeEvery, put } from 'redux-saga/effects';
 import logger from 'redux-logger';
 
 
-
-const templateReducer = (state=0, action) => {
+const templateReducer = (state = [], action) => {
     console.log('here is a reducer');
+    switch(action.type){
+        case 'SET_IMAGES':
+            return action.payload;
+            default:
     return state;
-}
+};//end reducer function*/
 
-/*start reducer function
+function* getImages (action){
+    try{
+        const imageResponse = yield axios.get(`/api/giphy/${action.payload}`);
+        yield put({type: 'SET_IMAGES', payload: imageResponse.data});
+    }catch(error){
+        console.log(error);
+    }
+}//end saga function*/
 
-************************
-
-end reducer function*/
-
-/*start saga function
-
-************************
-
-end saga function*/
-
-/*start saga function
-
-************************
-
-end saga function*/
+function* createImage(action){
+    try{
+        yield axios.post(`/api/giphy/${action.payload}`, {newImage: action.payload});
+        yield put ({type: 'GET_IMAGES'})
+    }catch(error){
+        console.log(error)
+    }
+}//end saga function*/
 
 // saga function to listen for actions
 function* watcherSaga() {
     console.log('in watcherSaga');
     //yield takeEvery(type, function);
+    yield takeEvery('GET_IMAGES', getImages);
+    yield takeEvery('CREATE_IMAGE', createImage)
+    
 }
 
 const sagaMiddleware = createSagaMiddleware();
