0c908c47fb0047962219fdfc21f20e755fbbdc7d
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-09-30 10:53:05 -0500

    created boilerplate saga, redux, and routing in index.js and app.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const templateReducer = (state=0, action) => {
+    console.log('here is a reducer');
+    return state;
+}
+
+/*start reducer function
+
+************************
+
+end reducer function*/
+
+/*start saga function
+
+************************
+
+end saga function*/
+
+/*start saga function
+
+************************
+
+end saga function*/
+
+// saga function to listen for actions
+function* watcherSaga() {
+    console.log('in watcherSaga');
+    //yield takeEvery(type, function);
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const reduxStore = createStore(
+    combineReducers({
+        templateReducer
+    }),
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
